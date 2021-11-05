878b098cab5ddc2fb1bc276dc9d1ab1c87e86c2a
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2021-11-05 21:27:46 +0300

    fix: CORS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -14,37 +14,13 @@ const NotFoundError = require('./errors/NotFoundError');
 const app = express();
 
 const { PORT = 3000 } = process.env;
-
-app.use('*', cors({
-  origin: 'https://ekaanikeeva.backend.nomoredomains.rocks',
-  credentials: true,
-}));
-// const allowedCors = [
-//   'https://ekaanikeeva.nomoredomains.rocks',
-//   'http://ekaanikeeva.nomoredomains.rocks',
-//   'http://localhost:3000',
-//   'https://ekaanikeeva.backend.nomoredomains.rocks',
-//   'http://ekaanikeeva.backend.nomoredomains.rocks',
-//   'https://ekaanikeeva.backend.nomoredomains.rocks/cards',
-//   'https://ekaanikeeva.backend.nomoredomains.rocks/users/me',
-
-// ];
-
-// const corsOption = {
+app.use(cors());
+// app.use('*', cors({
+//   origin: 'https://ekaanikeeva.backend.nomoredomains.rocks',
 //   credentials: true,
-//   origin: function checkCorsList(origin, callback) {
-//     if (allowedCors.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
+// }));
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors(corsOption));
-
 app.use(requestLogger);
 app.post('/signin', validateSignIn, login);
 app.post('/signup', validateSignIn, createUser);
