af4b4507acf30650a429dcd3e3d8b2082dafcfc6
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2021-11-05 23:02:08 +0300

    fix CORS

--- app.js
+++ app.js
@@ -13,11 +13,43 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 204,
-};
-app.use(express.json(), cors(corsOptions));
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200,
+// };
+app.use((req, res) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+// Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+}
+})
+// const allowedCors = [
+//   'https://ekaanikeeva.nomoredomains.rocks',
+//   'http://ekaanikeeva.nomoredomains.rocks',
+//   'https://ekaanikeeva.backend.nomoredomains.rocks',
+//   'http://ekaanikeeva.backend.nomoredomains.rocks',
+// ];
+
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//   } if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//   }
+//   next();
+// });
+app.use(express.json());
 
 app.use(cookieParser());
 
